72b811a59895c64c41ffed134b7ec31e01d2ccce
Kaiichiro Ota <kigh.ota@gmail.com>
2020-03-15 06:42:26 +0900

    .gitignore_global

--- unix.sh
+++ unix.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
 # ln -sf ~/dotfiles/.screenrc ~/.screenrc
+git config --global core.excludesfile ~/dotfiles/.gitignore_global
